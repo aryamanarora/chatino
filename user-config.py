6974c0044e1c2da9de5fe5530cbe01550d418d99
aryamanarora <aryaman.arora2020@gmail.com>
2020-08-16 00:18:47 -0400

    scripts and corpus

--- user-config.py
+++ user-config.py
@@ -4,3 +4,6 @@ mylang = 'en'
 usernames['wiktionary']['en'] = u'Aryamanbot'
 
 console_encoding = 'utf-8'
+
+minthrottle = 0
+maxthrottle = 1
--- wiktionary.py
+++ wiktionary.py
@@ -1,43 +1,114 @@
 import pywikibot, csv
+import stanza
 
 site = pywikibot.Site(code='en', fam='wiktionary')
 site.login()
 
-with open('data.csv', 'r') as fin:
+nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma')
+
+tones_to_letters = {
+    'MS': 'H',
+    'MH': 'I',
+    'ML': 'J',
+    'HL': 'B',
+    'HS': 'D',
+    'LS': 'M',
+    'LH': 'G',
+    'LM': 'F',
+    'H': 'E',
+    'M': 'C',
+    'L': 'A'
+}
+
+tones_to_numbers = {
+    'LS': '40',
+    'LH': '42',
+    'LM': '3',
+    'MS': '20',
+    'MH': '32',
+    'ML': '24',
+    'HS': '10',
+    'HL': '14',
+    'H': '1',
+    'M': '2',
+    'L': '4'
+}
+
+edits = 0
+with open('new corpus.csv', 'r') as fin:
     reader = csv.reader(fin)
     header = ['CPL', 'PROG', 'HAB', 'POT']
     cur = {}
     for i, data in enumerate(reader):
+        if i < 3: continue
         print(data)
-        if i < 4: continue
-        if data[2] == 'CPL': continue
         if data[0]:
             if data[0][0] == '[':
-                cur['class'] = data[0][1]
+                # class and headword
+                cur['class'] = data[0][1:-1]
                 cur['word'] = data[1]
-                cur['gloss'] = data[2]
-                cur['transitivity'] = ''
-                if 'iv' in data[3]: cur['transitivity'] += 'i'
-                if 'tv' in data[3]: cur['transitivity'] += 't'
-                cur['paradigm'] = []
-        elif cur and data[1]:
-            cur['paradigm'].extend(data[2:6])
-        if data[1] == '3;PL':
-            entry = f"""==Western Highland Chatino==
 
-===Verb===
-{{{{ctp-verb|{cur['word']}|class={cur['class']}|t={cur['transitivity']}}}}}
+                # altforms (using stupid find-replace)
+                cur['altnums'] = cur['word']
+                for key, val in tones_to_numbers.items():
+                    cur['altnums'] = cur['altnums'].replace(key, val)
+                cur['altletters'] = cur['word']
+                for key, val in tones_to_letters.items():
+                    cur['altletters'] = cur['altletters'].replace(key, val)
+
+                # other pertinent information
+                cur['gloss'] = data[2]
+                words = nlp(cur['gloss']).sentences[0].words
+                cur['gloss'] = ' '.join([f'[[{word.lemma}|{word.text}]]' for word in words])
+                print(cur['gloss'])
+                # cur['transitivity'] = ''
+                # if 'iv' in data[3]: cur['transitivity'] += 'i'
+                # if 'tv' in data[3]: cur['transitivity'] += 't'
+                cur['paradigm'] = []
+                for cell in data[4:8]:
+                    cur['paradigm'].extend([word.strip() for word in cell.split('\n')])
+
+                page = pywikibot.Page(site, cur['word'])
+                if '==San Juan Quiahije Chatino==' in page.text:
+
+                    match_text = f"""===References===
+* {{{{R:ctp-san:Cruz, et al. 2020}}}}"""
+                    pos = page.text.find(match_text)
+                    entry = f"""===Verb===
+{{{{ctp-san-verb|{cur['word']}|class={cur['class']}}}}}
 
 # {cur['gloss']}
 
 ====Conjugation====
-{{{{ctp-conj|class={cur['class']}|{'|'.join(cur['paradigm'])}}}}}
+{{{{ctp-san-conj|class={cur['class']}|{'|'.join(cur['paradigm'])}}}}}
+
+"""
+                    if entry not in page.text:
+                        page.text = page.text[:pos] + entry + page.text[pos:]
+                        page.save('San Juan Quiahije Chatino verb with inflection from file--existing lang section')
+                    edits += 1
+                else:
+                    entry = f"""
+==San Juan Quiahije Chatino==
+
+===Alternative forms===
+* {{{{alter|ctp-san|{cur['altnums']}||numeral tones}}}}
+* {{{{alter|ctp-san|{cur['altletters']}||alphabetic tones}}}}
+
+===Verb===
+{{{{ctp-san-verb|{cur['word']}|class={cur['class']}}}}}
+
+# {cur['gloss']}
+
+====Conjugation====
+{{{{ctp-san-conj|class={cur['class']}|{'|'.join(cur['paradigm'])}}}}}
 
 ===References===
-* {{{{R:ctp:Cruz, et al. 2020}}}}
-            """
-
-            page = pywikibot.Page(site, 'User:AryamanA/' + cur['word'])
-            page.text = entry
-            page.save('Western Highland Chatino verb with inflection from file')
+* {{{{R:ctp-san:Cruz, et al. 2020}}}}
+                """
+                    if entry not in page.text:
+                        page.text = page.text[:pos] + entry + page.text[pos:]
+                        page.save('San Juan Quije Chatino verb with inflection from file')
+                    edits += 1
+                print(edits)
 
